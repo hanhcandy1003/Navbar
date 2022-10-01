0424f705071371af85ed829bd360c0e07f174ef2
hanhcandy1003 <candyhayoni@gmail.com>
2022-10-01 21:57:47 +0700

    add file navbar

---
+++ materials/Home.js
@@ -0,0 +1,15 @@
+import styles from "./styles.css";
+
+const firstLetter = () => <h1 className={styles.bigwhite}>CUỘC CHIẾN</h1>;
+const secondLetter = () => <h1 className={styles.bigwhite}>VƯƠNG QUYỀN</h1>;
+
+export default Home = () => {
+  return (
+    <>
+      <firstLetter />;
+      <>
+        <secondLetter />;
+      </>
+    </>
+  );
+};
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
